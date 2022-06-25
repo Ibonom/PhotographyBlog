94bf6f7aa8e9a7773a976935b390f17ca7fa93e1
Ibonom <ibonomgame@gmail.com>
2022-06-25 20:43:31 +0200

    1/4 page ,without logic

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,17 @@ const { src, dest, watch } = require("gulp");
 const concat = require("gulp-concat");
 const sass = require("gulp-sass")(require("sass"));
 const minifyCss = require("gulp-clean-css");
-const sourceMaps = require('gulp-sourcemaps')
+const sourceMaps = require("gulp-sourcemaps");
+const browserSync = require("browser-sync").create();
+
 
 const buildStyles = () => {
-  return src("./src/Scss/**/*.scss").pipe(sourceMaps.init())
+  return src("./src/Scss/**/*.scss")
+    .pipe(sourceMaps.init())
     .pipe(sass().on("error", sass.logError))
-    .pipe(minifyCss()).pipe(sourceMaps.write())
+    .pipe(browserSync.stream())
+    .pipe(minifyCss({ processImport: false }))
+    .pipe(sourceMaps.write())
     .pipe(concat("bundle.css"))
     .pipe(dest("./public"));
 };
@@ -17,4 +22,5 @@ const buildStyles = () => {
 exports.buildStyles = buildStyles;
 exports.watch = () => {
   watch("./src/Scss/**/*.scss", buildStyles);
+  watch("./*.html").on("change", browserSync.reload);
 };
